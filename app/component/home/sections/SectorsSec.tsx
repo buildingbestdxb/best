574c28c6c29c9c3762b56ddf32ca53543b42f1b1
yasirtlr <yasirtlr@gmail.com>
2025-02-06 11:52:01 +0400

    sector

--- app/component/home/sections/SectorsSec.tsx
+++ app/component/home/sections/SectorsSec.tsx
@@ -80,7 +80,7 @@ const SectorsSec = () => {
           {sectors.map((sector, index) => (
             <motion.div
               key={sector.id}
-              className="relative h-[300px] lg:h-[400px] overflow-hidden rounded-custom shadow-lg group"
+              className="relative h-[300px] lg:h-[400px] overflow-hidden rounded-custom shadow-lg group cursor-pointer"
               initial={{ opacity: 0, y: 30 }}
               whileInView={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: index * 0.1 }}
