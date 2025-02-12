e1ec52374025773064ca29fda719be9d091ea2c1
sreemayispraseed <sreemayi99@gmail.com>
2025-02-12 12:17:05 +0530

    content updated

--- app/component/projectDetails/otherProjects.tsx
+++ app/component/projectDetails/otherProjects.tsx
@@ -10,19 +10,19 @@ const ProjectList = () => {
           <div className="grid md:grid-cols-2 xl:grid-cols-12 gap-[32px] items-center mt-5 lg:mt-[60px]">
             <div className="xl:col-span-4">
               <ProjectCard
-                title={"sgdhs"}
+                title={"3000 Prayer Mosque"}
                 image="/assets/img/projects/p-1.jpg"
               />
             </div>
             <div className="xl:col-span-4">
               <ProjectCard
-                title={"sgdhs"}
+                title={"22 Villas Al Khalidiya"}
                 image="/assets/img/projects/p-2.jpg"
               />
             </div>
             <div className="xl:col-span-4">
               <ProjectCard
-                title={"sgdhs"}
+                title={"Gardenia Residence"}
                 image="/assets/img/projects/p-3.jpg"
               />
             </div>
--- app/component/projectsLists/ProjectList.tsx
+++ app/component/projectsLists/ProjectList.tsx
@@ -10,43 +10,43 @@ const ProjectList = () => {
           <div className="grid md:grid-cols-2 xl:grid-cols-12 gap-[32px] items-center mt-5 lg:mt-[60px]">
             <div className="xl:col-span-8">
               <ProjectCard
-                title={"sgdhs"}
+                title={"3000 Prayer Mosque"}
                 image="/assets/img/projects/p-1.jpg"
               />
             </div>
             <div className="xl:col-span-4">
               <ProjectCard
-                title={"sgdhs"}
+                title={"Gardenia Residence"}
                 image="/assets/img/projects/p-2.jpg"
               />
             </div>
             <div className="xl:col-span-4">
               <ProjectCard
-                title={"sgdhs"}
+                title={"Al Ansari Residential Building – Noor Tower"}
                 image="/assets/img/projects/p-3.jpg"
               />
             </div>
             <div className="xl:col-span-4">
               <ProjectCard
-                title={"sgdhs"}
+                title={"Scientific Research Building – Sharjah University"}
                 image="/assets/img/projects/p-4.jpg"
               />
             </div>
             <div className="xl:col-span-4">
               <ProjectCard
-                title={"sgdhs"}
+                title={"Al Zahra Hospital Dubai"}
                 image="/assets/img/projects/p-5.jpg"
               />
             </div>
             <div className="xl:col-span-4">
               <ProjectCard
-                title={"sgdhs"}
+                title={"Gardenia Residence"}
                 image="/assets/img/projects/p-6.jpg"
               />
             </div>
             <div className="xl:col-span-8">
               <ProjectCard
-                title={"sgdhs"}
+                title={"3000 Prayer Mosque"}
                 image="/assets/img/projects/p-7.jpg"
               />
             </div>
