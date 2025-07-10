970045677d611ebae1590e1011b3903c63dd90a9
Nived <nived@globalsurf.ae>
2025-07-10 14:36:41 +0530

    test:pdf viewer

--- app/component/Accreditations/Certificates.tsx
+++ app/component/Accreditations/Certificates.tsx
@@ -115,7 +115,7 @@ const Certificates = () => {
                       </button>
 
             <div className="w-full h-full">
-              <Worker workerUrl={`https://unpkg.com/pdfjs-dist@3.11.174/build/pdf.worker.min.js`}>
+              <Worker workerUrl={`/pdf-worker/pdf.worker.min.js`}>
                 <Viewer fileUrl={sector.files[0].file} />
               </Worker>
             </div>
