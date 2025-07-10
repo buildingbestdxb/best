f9cf8bade01f447813cc1cdde17b90df50de23b2
Nived <nived@globalsurf.ae>
2025-07-10 14:17:14 +0530

    test:vercel build

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,15 @@ const nextConfig: NextConfig = {
     compiler:{
     removeConsole : process.env.NODE_ENV === 'production'
   },
+  webpack(config, { isServer }) {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        canvas: false, // Prevents trying to resolve 'canvas' in the browser
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
